128b82da23c2df10ae725a5457b19df63102ea5e
Vargola <vargola@hotmail.com>
2018-01-11 07:23:47 -0200

    first commit

---
+++ ImportaExcel/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ImportaExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPlanilha = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(16, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 49);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPlanilha
+            // 
+            this.txtPlanilha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPlanilha.Location = new System.Drawing.Point(77, 32);
+            this.txtPlanilha.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPlanilha.Name = "txtPlanilha";
+            this.txtPlanilha.Size = new System.Drawing.Size(860, 30);
+            this.txtPlanilha.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(765, 86);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 49);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Importar Planilha";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 692);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtPlanilha);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPlanilha;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ImportaExcel/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ImportaExcel
+{
+    public partial class Form1 : Form
+    {
+        const string STR_CONN = @"Password=s2u0r1f0;Persist Security Info=True;User ID=sa;Initial Catalog=db_eneagrama;Data Source=VARGOLA-PC\SQLEXPRESS14";
+
+        private Excel.Application xlApp = null;
+        private Excel.Workbooks xlWorkbooks = null;
+        private Excel.Workbook xlWorkbook = null;
+        private Excel.Sheets xlWorksheets = null;
+        private Excel._Worksheet xlWorksheet = null;
+        private Excel.Range xlRange = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtPlanilha.Text = "";
+
+            openFileDialog1.Title = "Selecionar planilha Excel";
+            openFileDialog1.InitialDirectory = @"h:\backup\wrk\eneagrama\planilhas";
+            openFileDialog1.Filter = "Excel (*.xls;*.xlsx)|*.xls;*.xlsx";
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+
+            DialogResult dr = openFileDialog1.ShowDialog();
+
+            if(dr == System.Windows.Forms.DialogResult.OK)
+            {
+                txtPlanilha.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (txtPlanilha.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("Informe a planilha");
+                txtPlanilha.Focus();
+                return;
+            }
+
+            xlApp = new Excel.Application();
+            xlWorkbooks = xlApp.Workbooks;
+            xlWorkbook = xlWorkbooks.Open(txtPlanilha.Text.Trim(),
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            xlWorksheets = xlWorkbook.Sheets;
+            xlWorksheet = null;
+            xlRange = null;
+
+            int rowIni = 2;
+
+            for(int i = rowIni; i <= xlWorksheets.Count; i++)
+            {
+                xlWorksheet = xlWorksheets[i];
+                xlRange = xlWorksheet.UsedRange;
+
+                int rowTurma = 1;
+                string ds_turma = getCell(xlRange, rowTurma, 1);
+                string id_curso = getCell(xlRange, rowTurma, 2);
+                string nr_ano = getCell(xlRange, rowTurma, 3);
+                string nr_mes = getCell(xlRange, rowTurma, 4);
+                string id_professor = getCell(xlRange, rowTurma, 5);
+                string ds_local = getCell(xlRange, rowTurma, 6);
+
+                string sql = @"insert into tb_turmas (ds_turma, id_curso, nr_ano, nr_mes, id_professor, ds_local)
+                        values (@ds_turma, @id_curso, @nr_ano, @nr_mes, @id_professor, @ds_local)";
+                List<SqlParameter> param = new List<SqlParameter>();
+                param.Add(new SqlParameter("@ds_turma", ds_turma));
+                param.Add(new SqlParameter("@id_curso", id_curso));
+                param.Add(new SqlParameter("@nr_ano", nr_ano));
+                param.Add(new SqlParameter("@nr_mes", nr_mes));
+                param.Add(new SqlParameter("@id_professor", id_professor));
+                param.Add(new SqlParameter("@ds_local", ds_local));
+                int id_turma = comandoSQL(sql, param);
+
+                int colNome = getCellToInt32(xlRange, rowTurma, 7);
+                int colEmail1 = getCellToInt32(xlRange, rowTurma, 8);
+                int colEmail2 = getCellToInt32(xlRange, rowTurma, 9);
+                int colEmail3 = getCellToInt32(xlRange, rowTurma, 10);
+                int colFone1 = getCellToInt32(xlRange, rowTurma, 11);
+                int colFone2 = getCellToInt32(xlRange, rowTurma, 12);
+                int colFone3 = getCellToInt32(xlRange, rowTurma, 13);
+
+                int c = 0;
+                for(int row = rowIni; row <= xlRange.Rows.Count; row++)
+                {
+                    c++;
+                    string key = "t" + id_turma + "-" + c;
+
+                    if (colNome > 0)
+                    {
+                        string nome = getCell(xlRange, row, colNome);
+
+                        sql = "insert into tb_alunos (nm_aluno, id_key_planilha) values ( " +
+                            "'" + nome.Trim().ToUpper() + "', " +
+                            key + " )";
+                        string idAluno = "7622";
+
+                    }
+
+                    for(col = 1; col <= xlRange.Columns.Count; col++)
+                    {
+                        var cell = xlRange.Cells[row, col];
+                        if (cell != null && cell.Value2 != null)
+                        {
+                            var linha = "[" + row + ", " + col + "] = " + cell.Value2;
+                            richTextBox1.AppendText("\n" + linha);
+                        }
+                    }
+                }
+
+                releaseComObject(xlRange);
+                xlRange = null;
+                releaseComObject(xlWorksheet);
+                xlWorksheet = null;
+            }
+
+            xlWorkbook.Close();
+            liberarProcessoExcel();
+            
+        }
+
+        private string getCell(Excel.Range xlRange, int row, int col)
+        {
+            var cell = xlRange.Cells[row, col];
+            if (cell != null && cell.Value2 != null)
+            {
+                return cell.ToString().Trim();
+            }
+            return "";
+        }
+
+        private int getCellToInt32(Excel.Range xlRange, int row, int col)
+        {
+            string cell = getCell(xlRange, row, col);
+            return cell.Equals(String.Empty) ? 0 : Convert.ToInt32(cell);
+        }
+
+        private void releaseComObject(object o)
+        {
+            if (o != null)
+            {
+                Marshal.ReleaseComObject(o);
+                o = null;
+            }
+        }
+
+        private void liberarProcessoExcel()
+        {
+            releaseComObject(xlRange);
+            xlRange = null;
+            releaseComObject(xlWorksheet);
+            xlWorksheet = null;
+            releaseComObject(xlWorksheets);
+            xlWorksheets = null;
+            releaseComObject(xlWorkbook);
+            xlWorkbook = null;
+            releaseComObject(xlWorkbooks);
+            xlWorkbooks = null;
+            xlApp.Quit();
+            releaseComObject(xlApp);
+            xlApp = null;
+        }
+
+        private int comandoSQL(string sql, List<SqlParameter> param)
+        {
+            SqlConnection conn = new SqlConnection(STR_CONN);
+            conn.Open();
+            SqlCommand comm = conn.CreateCommand();
+            comm.CommandText = sql;
+            comm.CommandType = CommandType.Text;
+            foreach(var p in param)
+            {
+                comm.Parameters.Add(p);
+            }
+            int result = comm.ExecuteNonQuery();
+            comm.Dispose();
+            conn.Close();
+            conn.Dispose();
+            return result;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            liberarProcessoExcel();
+        }
+    }
+}
